3d69079305f1ce456667a1b2b9c3b590a4876191
Ishika-07 <60807873+Ishika-07@users.noreply.github.com>
2022-06-13 20:48:15 +0530

    Style added

--- public/script.js
+++ public/script.js
@@ -1,68 +1,27 @@
 const socket = io("/");
 
-var peer = new Peer(undefined, {
-    path: "/peerjs",
-    host: "/",
-    port: "443",
-});
-
-const user = prompt("ENTER YOUR NAME");
-
-const video_element =document.createElement("video");
-video_element.muted = true;
-
-var video_streams
-navigator.mediaDevices.getUserMedia({audio:true, video:true}).then((stream)=>{
-    video_streams = stream
-
-    addVideoStream(video_element, stream);
-});
-
-function addVideoStream(element, stream){
-    element.srcObject = stream;
-    element.addEventListener("loadmetadata", ()=>{
-        element.play()
-        $("video_stream").append(element)
-    })
-}
+const user = prompt("Enter your name");
 
 $(function () {
-    $("#show_chat").click(function () {
-        $(".left-window").css("display", "none")
-        $(".right-window").css("display", "block")
-        $(".header_back").css("display", "block")
-    })
-    $(".header_back").click(function () {
-        $(".left-window").css("display", "block")
-        $(".right-window").css("display", "none")
-        $(".header_back").css("display", "none")
-    })
-
     $("#send").click(function () {
         if ($("#chat_message").val().length !== 0) {
-            socket.emit("message", $("#chat_message").val());
+            console.log(user);
+            socket.emit("message", $("#chat_message").val(), user);
             $("#chat_message").val("");
         }
     })
-
     $("#chat_message").keydown(function (e) {
         if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
-            socket.emit("message", $("#chat_message").val());
+            socket.emit("message", $("#chat_message").val(), user);
             $("#chat_message").val("");
         }
     })
-
 })
-
-peer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id, user);
-});
 
 socket.on("createMessage", (message, userName) => {
     $(".messages").append(`
         <div class="message">
-            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
-        }</span> </b>
+            <b><span class="username"> ${userName}: </span> </b>
             <span>${message}</span>
         </div>
     `)
